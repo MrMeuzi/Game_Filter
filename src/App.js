f79f69aac753d2b500c97bfa65310fe7c2f8953e
_MrMeuzi_ <mrmeuzi56@gmail.com>
2025-11-26 22:32:42 +0500

    Начал делать навигатор, придумал дизайн логотипа и избранного.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import './styles/styles.css';
+import searchBarGlobal from './components/searchBarGlobal.jsx';
+import home from './pages/home.jsx';
+import favorites from './pages/favorites.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="nav__wrapper">
+        <nav className="nav__container">
+          <Link to="/" className="nav__logo"></Link>
+          <searchBarGlobal  />
+          <Link to="/favorites" className="nav__favorites"></Link>
+          <Routes>
+            <Route path="/" element={<home />} />
+            <Route path="/favorites" element={<favorites />} />
+          </Routes>
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// Заметка: nav__logo, nav_favorites, nav__wrapper, nav__container - классы в блоке навигации. 
